6a2c4e06d04219c5f2637939b7758c9e9a1388e1
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-11-05 14:51:44 -0400

    Add a comment explaining why a line is commented out
    Co-authored-by: dav3r <david.redmin@trio.dhs.gov>

--- setup.py
+++ setup.py
@@ -84,7 +84,8 @@ setup(
     keywords="aws, guacamole, vpc",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    # package_data={"guacamole-connection-scanner": ["data/*.txt"]},
+    # This package contains no data
+    # package_data={"guacscanner": ["data/*.txt"]},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     install_requires=[
