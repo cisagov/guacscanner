cd170b4183c0b0838d85ad4ad5f9aeb53dba2c20
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-11-10 12:41:56 -0500

    Get rid of need for noqa in one line

--- src/guacscanner/__init__.py
+++ src/guacscanner/__init__.py
@@ -1,10 +1,22 @@
 """The guacscanner library."""
-# We disable a Flake8 check for "Module imported but unused (F401)" here because
-# although this import is not directly used, it populates the value
-# package_name.__version__, which is used to get version information about this
-# Python package.
-from . import guacscanner  # noqa: F401
+# We disable a Flake8 check for "Module imported but unused (F401)"
+# here because, although this import is not directly used, it
+# populates the value package_name.__version__, which is used to get
+# version information about this Python package.
 from ._version import __version__  # noqa: F401
+from .guacscanner import (
+    add_instance_connection,
+    add_user,
+    check_for_ghost_instances,
+    entity_exists,
+    get_connection_name,
+    get_entity_id,
+    instance_connection_exists,
+    main,
+    process_instance,
+    remove_connection,
+    remove_instance_connections,
+)
 
 __all__ = [
     "add_instance_connection",
@@ -17,5 +29,5 @@ __all__ = [
     "main",
     "process_instance",
     "remove_connection",
-    "remove_instance_connection",
+    "remove_instance_connections",
 ]
