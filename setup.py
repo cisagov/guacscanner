4d4b2fa207ecee29b636ae64255544eba253460f
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-10-14 11:36:24 -0400

    Upgrade to the latest release of psycopg (3.2.10)
    This is the first version that supports Python 3.14.

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
         "boto3 == 1.34.108",
         "docopt == 0.6.2",
         "ec2-metadata == 2.13.0",
-        "psycopg[binary] == 3.2.9",
+        "psycopg[binary] == 3.2.10",
         "schema == 0.7.7",
         "setuptools",
     ],
