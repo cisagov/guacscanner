74bb4dd8abe4e36d173079a5417420b25f7efb5b
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-04-23 15:30:56 -0400

    Remove single quotes around ID field value
    In SQL strings should be enclosed in single quotes (in case they
    contain whitespace) but numeric value must not.

--- src/guacscanner/guacscanner.py
+++ src/guacscanner/guacscanner.py
@@ -140,7 +140,7 @@ INSERT_CONNECTION_QUERY = psycopg.sql.SQL(
 UPDATE_CONNECTION_NAME_QUERY = psycopg.sql.SQL(
     """UPDATE {table}
     SET {name_field} = '%s'
-    WHERE {id_field} = '%s';"""
+    WHERE {id_field} = %s;"""
 ).format(
     table=psycopg.sql.Identifier("guacamole_connection"),
     name_field=psycopg.sql.Identifier("connection_name"),
