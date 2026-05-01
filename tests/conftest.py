14c4b6eeb02e2e68b5b1329866220db3281ea8a4
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-04-30 22:42:17 -0400

    Add Docker composition for testing this Python code
    The Docker composition is based on that in
    cisagov/guacamole-composition but is altered to allow an instance of
    guacscanner running on the Docker host to access the mock AWS IMDS
    service and the PostgreSQL instance.
    
    Note that we also have to add a library to the test requirements of
    the Python library and set up some test fixtures.  We also have to
    pre-populate some dummy secrets used by the Docker composition.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,39 @@ https://docs.pytest.org/en/latest/writing_plugins.html#conftest-py-plugins
 
 # Third-Party Libraries
 import pytest
+from python_on_whales import docker
+
+
+@pytest.fixture(scope="session")
+def dockerc():
+    """Start up the Docker composition."""
+    docker.compose.up(detach=True)
+    yield docker
+    # Since this Docker composition includes data volumes, we want to
+    # remove volumes as well when we bring the composition down so we
+    # start from a clean slate next time.
+    docker.compose.down(volumes=True)
+
+
+@pytest.fixture(scope="session")
+def guacamole_container(dockerc):
+    """Return the guacamole container from the Docker composition."""
+    # find the container by name even if it is stopped already
+    return dockerc.compose.ps(services=["guacamole"], all=True)[0]
+
+
+@pytest.fixture(scope="session")
+def guacd_container(dockerc):
+    """Return the guacd container from the Docker composition."""
+    # find the container by name even if it is stopped already
+    return dockerc.compose.ps(services=["guacd"], all=True)[0]
+
+
+@pytest.fixture(scope="session")
+def postgres_container(dockerc):
+    """Return the postgres container from the Docker composition."""
+    # find the container by name even if it is stopped already
+    return dockerc.compose.ps(services=["postgres"], all=True)[0]
 
 
 def pytest_addoption(parser):
