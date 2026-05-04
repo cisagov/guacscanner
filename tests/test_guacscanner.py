edca90c5ca5aec23005fb4281c4aaa59148a961e
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-04 10:41:34 -0400

    Group related tests in classes

--- tests/test_guacscanner.py
+++ tests/test_guacscanner.py
@@ -33,144 +33,153 @@ PROJECT_VERSION = guacscanner.__version__
 DUMMY_VPC_ID = "vpc-0123456789abcdef0"
 
 
-def test_stdout_version(capsys):
-    """Verify that version string sent to stdout agrees with the module version."""
-    with pytest.raises(SystemExit):
-        with patch.object(sys, "argv", ["bogus", "--version"]):
-            guacscanner.guacscanner.main()
-    captured = capsys.readouterr()
-    assert (
-        captured.out == f"{PROJECT_VERSION}\n"
-    ), "standard output by '--version' should agree with module.__version__"
+class TestVersion:
+    """Tests related to project version."""
+
+    def test_stdout_version(self, capsys):
+        """Verify that version string sent to stdout agrees with the module version."""
+        with pytest.raises(SystemExit):
+            with patch.object(sys, "argv", ["bogus", "--version"]):
+                guacscanner.guacscanner.main()
+        captured = capsys.readouterr()
+        assert (
+            captured.out == f"{PROJECT_VERSION}\n"
+        ), "standard output by '--version' should agree with module.__version__"
+
+    def test_running_as_module(self, capsys):
+        """Verify that the __main__.py file loads correctly."""
+        with pytest.raises(SystemExit):
+            with patch.object(sys, "argv", ["bogus", "--version"]):
+                # F401 is a "Module imported but unused" warning. This import
+                # emulates how this project would be run as a module. The only thing
+                # being done by __main__ is importing the main entrypoint of the
+                # package and running it, so there is nothing to use from this
+                # import. As a result, we can safely ignore this warning.
+                # cisagov Libraries
+                import guacscanner.__main__  # noqa: F401
+        captured = capsys.readouterr()
+        assert (
+            captured.out == f"{PROJECT_VERSION}\n"
+        ), "standard output by '--version' should agree with module.__version__"
+
+    @pytest.mark.skipif(
+        RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
+    )
+    def test_release_version(self):
+        """Verify that release tag version agrees with the module version."""
+        assert (
+            RELEASE_TAG == f"v{PROJECT_VERSION}"
+        ), "RELEASE_TAG does not match the project version"
 
 
-def test_running_as_module(capsys):
-    """Verify that the __main__.py file loads correctly."""
-    with pytest.raises(SystemExit):
-        with patch.object(sys, "argv", ["bogus", "--version"]):
-            # F401 is a "Module imported but unused" warning. This import
-            # emulates how this project would be run as a module. The only thing
-            # being done by __main__ is importing the main entrypoint of the
-            # package and running it, so there is nothing to use from this
-            # import. As a result, we can safely ignore this warning.
-            # cisagov Libraries
-            import guacscanner.__main__  # noqa: F401
-    captured = capsys.readouterr()
-    assert (
-        captured.out == f"{PROJECT_VERSION}\n"
-    ), "standard output by '--version' should agree with module.__version__"
+class TestLogLevels:
+    """Tests related to setting the log level."""
 
+    @pytest.mark.parametrize("level", LOG_LEVELS)
+    @pytest.mark.usefixtures("dockerc")
+    def test_log_levels(self, level):
+        """Validate commandline log-level arguments."""
+        with patch.object(
+            sys,
+            "argv",
+            [
+                f"--log-level={level}",
+                "--oneshot",
+                "--postgres-hostname=localhost",
+                "--postgres-password-file=src/secrets/postgres-password",
+                "--postgres-username-file=src/secrets/postgres-username",
+                "--private-ssh-key=dummy_key",
+                "--rdp-password=dummy_rdp_password",
+                "--rdp-username=dummy_rdp_username",
+                "--vnc-password=dummy_vnc_password",
+                "--vnc-username=dummy_vnc_username",
+                f"--vpc-id={DUMMY_VPC_ID}",
+                "--windows-sftp-base=/C:/Users/dummy_user",
+            ],
+        ):
+            with patch.object(logging.root, "handlers", []):
+                assert (
+                    logging.root.hasHandlers() is False
+                ), "root logger should not have handlers yet"
+                return_code = None
+                try:
+                    guacscanner.guacscanner.main()
+                except SystemExit as sys_exit:
+                    return_code = sys_exit.code
+                    assert return_code is None, "main() should return success"
+                    assert (
+                        logging.root.hasHandlers() is True
+                    ), "root logger should now have a handler"
+                    assert (
+                        logging.getLevelName(logging.root.getEffectiveLevel())
+                        == level.upper()
+                    ), f"root logger level should be set to {level.upper()}"
+                    assert return_code is None, "main() should return success"
 
-@pytest.mark.skipif(
-    RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
-)
-def test_release_version():
-    """Verify that release tag version agrees with the module version."""
-    assert (
-        RELEASE_TAG == f"v{PROJECT_VERSION}"
-    ), "RELEASE_TAG does not match the project version"
-
-
-@pytest.mark.parametrize("level", LOG_LEVELS)
-def test_log_levels(level, postgres_container):
-    """Validate commandline log-level arguments."""
-    with patch.object(
-        sys,
-        "argv",
-        [
-            f"--log-level={level}",
-            "--oneshot",
-            "--postgres-hostname=localhost",
-            "--postgres-password-file=src/secrets/postgres-password",
-            "--postgres-username-file=src/secrets/postgres-username",
-            "--private-ssh-key=dummy_key",
-            "--rdp-password=dummy_rdp_password",
-            "--rdp-username=dummy_rdp_username",
-            "--vnc-password=dummy_vnc_password",
-            "--vnc-username=dummy_vnc_username",
-            f"--vpc-id={DUMMY_VPC_ID}",
-            "--windows-sftp-base=/C:/Users/dummy_user",
-        ],
-    ):
-        with patch.object(logging.root, "handlers", []):
-            assert (
-                logging.root.hasHandlers() is False
-            ), "root logger should not have handlers yet"
+    def test_bad_log_level(self):
+        """Validate bad log-level argument returns error."""
+        with patch.object(sys, "argv", ["bogus", "--log-level=emergency"]):
             return_code = None
             try:
                 guacscanner.guacscanner.main()
             except SystemExit as sys_exit:
                 return_code = sys_exit.code
-                assert return_code is None, "main() should return success"
-                assert (
-                    logging.root.hasHandlers() is True
-                ), "root logger should now have a handler"
-                assert (
-                    logging.getLevelName(logging.root.getEffectiveLevel())
-                    == level.upper()
-                ), f"root logger level should be set to {level.upper()}"
-                assert return_code is None, "main() should return success"
-
-
-def test_bad_log_level():
-    """Validate bad log-level argument returns error."""
-    with patch.object(sys, "argv", ["bogus", "--log-level=emergency"]):
-        return_code = None
-        try:
-            guacscanner.guacscanner.main()
-        except SystemExit as sys_exit:
-            return_code = sys_exit.code
-        assert return_code == 1, "main() should exit with error"
+            assert return_code == 1, "main() should exit with error"
 
 
 @mock_aws
-def test_addition_of_guacuser(postgres_container, postgres_db_name, postgres_username):
-    """Verify that adding the guacuser works as expected."""
-    # Create a VPC
-    ec2 = boto3.client("ec2", "us-east-1")
-    vpc = ec2.create_vpc(CidrBlock="10.19.74.0/24")
-    vpc_id = vpc["Vpc"]["VpcId"]
+class TestGuacuser:
+    """Tests related to the addition of the guacuser."""
 
-    with patch.object(
-        sys,
-        "argv",
-        [
-            "--log-level=debug",
-            "--oneshot",
-            "--postgres-hostname=localhost",
-            "--postgres-password-file=src/secrets/postgres-password",
-            "--postgres-username-file=src/secrets/postgres-username",
-            "--private-ssh-key=dummy_key",
-            "--rdp-password=dummy_rdp_password",
-            "--rdp-username=dummy_rdp_username",
-            "--vnc-password=dummy_vnc_password",
-            "--vnc-username=dummy_vnc_username",
-            f"--vpc-id={vpc_id}",
-            "--windows-sftp-base=/C:/Users/dummy_user",
-        ],
+    def test_addition_of_guacuser(
+        self, postgres_container, postgres_db_name, postgres_username
     ):
-        guacscanner.guacscanner.main()
+        """Verify that adding the guacuser works as expected."""
+        # Create a VPC
+        ec2 = boto3.client("ec2", "us-east-1")
+        vpc = ec2.create_vpc(CidrBlock="10.19.74.0/24")
+        vpc_id = vpc["Vpc"]["VpcId"]
 
-        response = postgres_container.execute(
-            command=[
-                "psql",
-                "--command=SELECT name FROM guacamole_entity;",
-                f"--dbname={postgres_db_name}",
-                f"--username={postgres_username}",
-            ]
-        )
-        assert "guacadmin" in response
-        assert "guacuser" in response
+        with patch.object(
+            sys,
+            "argv",
+            [
+                "--log-level=debug",
+                "--oneshot",
+                "--postgres-hostname=localhost",
+                "--postgres-password-file=src/secrets/postgres-password",
+                "--postgres-username-file=src/secrets/postgres-username",
+                "--private-ssh-key=dummy_key",
+                "--rdp-password=dummy_rdp_password",
+                "--rdp-username=dummy_rdp_username",
+                "--vnc-password=dummy_vnc_password",
+                "--vnc-username=dummy_vnc_username",
+                f"--vpc-id={vpc_id}",
+                "--windows-sftp-base=/C:/Users/dummy_user",
+            ],
+        ):
+            guacscanner.guacscanner.main()
 
-        response = postgres_container.execute(
-            command=[
-                "psql",
-                "--command=SELECT COUNT(*) FROM guacamole_user;",
-                f"--dbname={postgres_db_name}",
-                f"--username={postgres_username}",
-            ]
-        )
-        assert "(1 row)" in response
+            response = postgres_container.execute(
+                command=[
+                    "psql",
+                    "--command=SELECT name FROM guacamole_entity;",
+                    f"--dbname={postgres_db_name}",
+                    f"--username={postgres_username}",
+                ]
+            )
+            assert "guacadmin" in response
+            assert "guacuser" in response
+
+            response = postgres_container.execute(
+                command=[
+                    "psql",
+                    "--command=SELECT COUNT(*) FROM guacamole_user;",
+                    f"--dbname={postgres_db_name}",
+                    f"--username={postgres_username}",
+                ]
+            )
+            assert "(1 row)" in response
 
 
 # @mock_aws
