7070f016f5fff80478b3bb56cc341af9f8cb4052
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-21 13:17:49 -0400

    Fix word-wrapping for docopt comment
    When the "[default: ...]" test is broken across a line the default
    value is ignored by docopt.  The test added in the previous commit
    fails without this change and passes with it.

--- src/guacscanner/guacscanner.py
+++ src/guacscanner/guacscanner.py
@@ -24,8 +24,7 @@ Options:
   -h --help              Show this message.
   --log-level=LEVEL    If specified, then the log level will be set to
     the specified value.  Valid values are "notset", "debug", "info",
-    "warning", "warn", "error", "fatal", and "critical". [default:
-    info]
+    "warning", "warn", "error", "fatal", and "critical". [default: info]
   --oneshot    If present then the loop that adds (removes)
     connections for new (terminated) instances will only be run once.
   --postgres-hostname=HOSTNAME    If specified then the specified
@@ -36,14 +35,12 @@ Options:
     PostgreSQL database.  Otherwise, the password will be read from a
     local file.
   --postgres-password-file=FILENAME    The file from which the
-    PostgreSQL password will be read. [default:
-    /run/secrets/postgres-password]
+    PostgreSQL password will be read. [default: /run/secrets/postgres-password]
   --postgres-username=USERNAME    If specified then the specified
     value will be used when connecting to the PostgreSQL database.
     Otherwise, the username will be read from a local file.
   --postgres-username-file=FILENAME    The file from which the
-    PostgreSQL username will be read. [default:
-    /run/secrets/postgres-username]
+    PostgreSQL username will be read. [default: /run/secrets/postgres-username]
   --private-ssh-key=KEY  If specified then the specified value will be
     used for the private SSH key.  Otherwise, the SSH key will be read
     from a local file.
@@ -80,8 +77,8 @@ Options:
     will be used as the base path for configuring Windows SFTP
     connections.  Otherwise, the path will be read from a local file.
   --windows-sftp-base-file=FILENAME    The file from which the base
-    path for Windows SFTP connections will be read. [default:
-    /run/secrets/windows-sftp-base]
+    path for Windows SFTP connections will be read.
+    [default: /run/secrets/windows-sftp-base]
 """
 
 # Standard Python Libraries
